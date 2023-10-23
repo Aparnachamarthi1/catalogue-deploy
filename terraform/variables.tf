2f630ef6d4f3985b1eeefbd0c9e076f3d8301e9f
Aparna <chamirthiaparna.com>
2023-10-23 13:57:26 +0530

    deploy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "common_tags" {
 #   default = "chamarthiaparna.online"
 # }
 
-# variable "app_version" {
-#   # this is just to test variable is flowing from terraform to shell and then to ansible
-#   default = "100.100.100"
-# }
+variable "app_version" {
+  # this is just to test variable is flowing from terraform to shell and then to ansible
+  default = "100.100.100"
+}
